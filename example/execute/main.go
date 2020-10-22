a5d1b7e10db51c7fd3a66e3c5a4c3e461b1ef3b3
Shailesh Suryawanshi <ss.shailesh28@gmail.com>
2020-10-22 01:46:24 -0400

    <Doc>:Added Examples and improved readme
    * Added Examples for ExecuteInto and Execute
    * Improved readme with lint
    
    author :ZachPlunkett <Zach13plunkett@gmail.com>

---
+++ example/execute/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	"github.com/ShaileshSurya/hammer"
+)
+
+func main() {
+	client := hammer.New()
+
+	request, err := hammer.RequestBuilder().
+		Get().
+		WithURL("http://echo.jsontest.com/Greeting/hello/place/world").
+		WithContext(context.Background()).
+		WithHeaders("Accept", "application/json").
+		Build()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Execute the request and manually handle response
+	resp, err := client.Execute(request)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	type HelloWorld struct {
+		Greeting string
+		Place    string
+	}
+	var hw HelloWorld
+
+	err = json.Unmarshal(body, &hw)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%+v\n", hw)
+}
